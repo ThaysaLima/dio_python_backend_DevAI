abc63750c3f3faa4da3e86db791c4f012f90b8d3
Thaysa Lima <limathaysajob@gmail.com>
2024-06-05 21:01:38 +0100

    Maneiras de usar o print()

--- curso_python_backend_DevAI/python_essencial_e_desenvolvimento_colaborativo_git/2conhecendo_a_linguagem_de_programacao_com_python/funcoes_de_entrada_e_saida.py
+++ curso_python_backend_DevAI/python_essencial_e_desenvolvimento_colaborativo_git/2conhecendo_a_linguagem_de_programacao_com_python/funcoes_de_entrada_e_saida.py
@@ -3,3 +3,14 @@
 nome = input("Informe seu nome: ")
 print(f"O seu nome é {nome}") # o nome vai ser substituido pelo o que o usuário responder em cima
 
+#MANEIRAS DE USAR O PRINT()
+nome = "Guilherme"
+sobrenome = "Carvalho"
+
+print(nome, sobrenome) #Guilherme Carvalho
+print(nome, sobrenome, end="...\n") #Guilherme Carvalho... 
+                                    #
+
+                                    #por padrão o end= já faz a quebra da linha \n, mas como foi modificado tem que colocar dnv. 
+print(nome, sobrenome, sep=" # ") #Guilherme # Carvalho
+                                #Por padrão o sep= faz a separação com 1 espaço para cada lado, mas como foi modificado temos que colocar dnv. 
