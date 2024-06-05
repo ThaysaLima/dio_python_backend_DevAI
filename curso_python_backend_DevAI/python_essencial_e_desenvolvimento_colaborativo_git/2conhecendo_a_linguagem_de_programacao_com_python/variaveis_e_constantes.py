009a44f807290720578971f87bd273ca33a3dfab
Thaysa Lima <limathaysajob@gmail.com>
2024-06-05 20:05:44 +0100

    atualização sobre snake case

---
+++ curso_python_backend_DevAI/python_essencial_e_desenvolvimento_colaborativo_git/2conhecendo_a_linguagem_de_programacao_com_python/variaveis_e_constantes.py
@@ -0,0 +1,13 @@
+idade = 23 #variavel
+print(idade)
+
+DEBUG = True #constante, convenção usada pelos programadores. 
+
+
+nome = "thaysa"
+idade = 28
+
+print(nome, idade)
+
+limite_saque_diario = 1000 
+limteSaqueDiario = 100 #usado tb, mas nem sempre em python
