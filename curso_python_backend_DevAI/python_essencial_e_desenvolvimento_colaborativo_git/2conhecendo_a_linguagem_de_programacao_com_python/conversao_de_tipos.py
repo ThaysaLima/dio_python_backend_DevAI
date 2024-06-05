644ad2b45643bfdde658f35daac34337aa0fef81
Thaysa Lima <limathaysajob@gmail.com>
2024-06-05 20:37:29 +0100

    os tipos de conversões

--- curso_python_backend_DevAI/python_essencial_e_desenvolvimento_colaborativo_git/2conhecendo_a_linguagem_de_programacao_com_python/conversao_de_tipos.py
+++ curso_python_backend_DevAI/python_essencial_e_desenvolvimento_colaborativo_git/2conhecendo_a_linguagem_de_programacao_com_python/conversao_de_tipos.py
@@ -1,2 +1,19 @@
 #Aula 4
 
+#CONVERSÃO PARA TIPO NUMÉRICO PARA STR 
+#Serve muito para formatação de texto 
+
+idade = 20 
+nome = "Thaysa"
+texto = f"A idade de {idade} anos é da {nome}"
+#Essa saída irá dar erro, pois a formatação só aceita strings, e na {idade} é um int. Então fazemos a conversão. 
+
+idade = str(idade)
+texto = f"A idade de {idade} anos é da {nome}"
+print(texto)
+
+#numa conversão do tipo float para int, ele sempre irá arredondar para baixo. 
+
+numero_float = 10.45
+numero_int = int(numero_float)
+print(numero_int) #saída = 10
