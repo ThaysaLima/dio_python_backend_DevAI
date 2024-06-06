3efac13d31eb867a0be57fc12ddef2bbadc1d09a
Thaysa Lima <limathaysajob@gmail.com>
2024-06-06 23:10:30 +0100

    Aula 01

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/1tipos_de_peredores_python/operadores_aritmeticos.py
@@ -0,0 +1,14 @@
+# Aula 01
+
+a, b = 1, 3
+
+print(a+b) #adição 
+print(a-b) #subtração
+print(a*b) #multiplicação
+
+print(a/b) #divisão
+print(a//b) #divisão inteira, sempre irá arredondar para baixo. 
+
+print(a%b) #módulo
+print(a**b) #exponenciação
+
