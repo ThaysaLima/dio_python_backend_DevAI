e817ad8da16468fced454e40b7b2eb026f7e4cdb
Thaysa Lima <limathaysajob@gmail.com>
2024-06-08 09:59:44 +0100

    exemplos de operadores de comparação

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/1tipos_de_peredores_python/operadores_comparacao.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/1tipos_de_peredores_python/operadores_comparacao.py
@@ -1,1 +1,19 @@
 # Aula 02
+
+saldo = 200
+saque = 200
+
+#igualdade
+print(saldo == saque)
+
+#diferença 
+print(saldo != saque)
+
+#maior/maior ou igual 
+print(saldo > saque)
+print(saldo >= saque)
+
+#menor/menor ou igual 
+print(saldo < saque)
+print(saldo <= saque)
+
