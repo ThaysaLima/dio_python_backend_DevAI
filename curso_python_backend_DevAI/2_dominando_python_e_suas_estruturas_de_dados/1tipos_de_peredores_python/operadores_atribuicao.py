20af4490255aee065c198a016c7aee16434ae71f
Thaysa Lima <limathaysajob@gmail.com>
2024-06-08 23:42:02 +0100

    aula 3

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/1tipos_de_peredores_python/operadores_atribuicao.py
@@ -0,0 +1,18 @@
+# Aula 03
+saldo = 500 #atribuição simples
+
+saldo += 200 #atribuição com adição >>>700
+    #isso significa que irá ser acrescentado mais 200 na variável inicial. 
+
+saldo -= 100 #atribuição com subtração >>>400
+
+saldo *= 2 #atribuição de multiplicação >>> 1000
+
+saldo /= 5 #atribuição de divisão >>> 100
+
+saldo //= 5 #atribuição de divisão inteira >>> 100
+
+saldo %= 480 #atribuição com módulo, resto da divisão de 500/480 >>> 20
+
+saldo2 = 80 
+saldo **= 2 #atribuição com exponênciação >>>6400
