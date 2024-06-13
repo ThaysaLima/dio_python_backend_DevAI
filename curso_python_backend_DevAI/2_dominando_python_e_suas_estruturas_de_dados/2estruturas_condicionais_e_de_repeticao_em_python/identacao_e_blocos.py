87f1d0ab5a85009de8162c28a689e624ed0b3659
Thaysa Lima <limathaysajob@gmail.com>
2024-06-13 17:22:36 +0100

    Aula 01

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/identacao_e_blocos.py
@@ -0,0 +1,14 @@
+# Aula 01 
+def sacar(valor): 
+    saldo = 500
+    if saldo >= valor:
+        print("Valor sacado")
+    else: 
+        print("Valor inválido")
+
+
+def depositat(valor): 
+    saldo = 500
+    saldo = valor
+
+sacar(70)
