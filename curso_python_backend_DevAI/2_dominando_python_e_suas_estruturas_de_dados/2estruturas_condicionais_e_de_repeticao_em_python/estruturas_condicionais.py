746abff0dd938f132be16e03083557135adcbbca
Thaysa Lima <limathaysajob@gmail.com>
2024-06-13 17:36:47 +0100

    Exemplo de IF

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/estruturas_condicionais.py
@@ -0,0 +1,10 @@
+# Aula 02
+#if 
+idade = int(input("Por favor insira a sua idade: "))
+
+if idade >= 18:
+    print("Você está habilitado a tirar a carteira de motorista!")
+
+if idade < 18: 
+    print("Para tirar a carteira de motorista você deve ter no mínimo 18 anos.")
+
