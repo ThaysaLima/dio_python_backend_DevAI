d4a0d7699962fd8ac9ceddc057de2cd21bb459ef
Thaysa Lima <limathaysajob@gmail.com>
2024-06-15 18:50:33 +0100

    Aula 02

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/estruturas_condicionais.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/estruturas_condicionais.py
@@ -8,3 +8,25 @@ if idade >= 18:
 if idade < 18: 
     print("Para tirar a carteira de motorista você deve ter no mínimo 18 anos.")
 
+#if e else
+
+opcao = int(input("Insira sua idade: "))
+
+if opcao == str: 
+    print("Por favor apresente apenas valores numéricos!")
+
+else:
+    print(f"Sua idade é {opcao} anos")
+
+#if, elif e else
+
+opcao = int(input("Informe o número da opção: [1] Sacar \n[2] Extrato: "))
+
+if opcao == 1: 
+    int(input("Insira a quantidade que deseja sacar: "))
+
+elif opcao == 2: 
+    print("Exibindo o extrato...")
+
+else:
+    print("Por favor, tente novamente e insira uma opção válida.")
