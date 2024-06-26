6547775155c4d261649af9b80e5383a436cec185
Thaysa Lima <limathaysajob@gmail.com>
2024-06-26 02:38:22 +0100

    Utilizando o break e o continue, Aula 3.3

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/estrutura_de_repeticao_break.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/2estruturas_condicionais_e_de_repeticao_em_python/estrutura_de_repeticao_break.py
@@ -1,1 +1,26 @@
 #Aula 3.3, break
+#Ideia, vamos fazer um sistema onde pediremos ao usuário colocar um número, para esse sistema começar a rodar ele deve ser diferente de zero, se for um número ímpar irá exibir um número, se for um número par ira exibir um outro número. Se o número for 10, irá interroper o sistema. 
+
+# while True: 
+#     numero = int(input("Coloque um número: "))
+
+#     if numero == 10: 
+#         break
+
+#     print(numero)
+
+#outro exemplo
+
+for numero in range(100):
+    if numero == 45:
+        break
+    print(numero, end=" ")
+
+#continue, serve para pular uma condição
+#aqui no caso, vai pular o 45...
+
+for numero in range(100):
+    if numero == 45:
+        continue
+
+    print(numero, end=" ")
