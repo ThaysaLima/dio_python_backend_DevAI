842d17aa5b2ecfedf63b4432fb8299d4b8e31b50
Thaysa Lima <limathaysajob@gmail.com>
2024-06-30 23:49:45 +0100

    Aula 3, 3ª maneira de formatação.

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/interacao_de_variaveis.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/interacao_de_variaveis.py
@@ -6,8 +6,11 @@ idade = 20
 profissao = "Programadora"
 cidade = "Fortaleza-CE"
 
-print("Olá, eu sou a %s e tenho %d. Nasci na cidade de %s, e atualmente atuo como %s" %(nome, idade, cidade, profissao)) # %s para valores em string, %d para valores em inteiro, %f para valores em flutuante. 
+print("Olá, eu sou a %s e tenho %d. Nasci na cidade de %s e atualmente atuo como %s" %(nome, idade, cidade, profissao)) # %s para valores em string, %d para valores em inteiro, %f para valores em flutuante. 
 
 #2ª maneira, format. 
 
-print("Olá, e chamo {} e tenho {}. Nasci na cidade de {}, e atualmente atuo como {}".format(nome, idade, cidade, profissao))
+print("Olá, e chamo {} e tenho {}. Nasci na cidade de {} e atualmente atuo como {}".format(nome, idade, cidade, profissao))
+
+#3ª maneira, f-string e a mais utilizada. 
+print(f"Olá, meu nome é {nome}, tenho {idade} anos. Nasci na cidade de {cidade} e atualmente atuo como {profissao}")
