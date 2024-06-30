9e60562b1654a7e34a3f082b7faa73cd8d268881
Thaysa Lima <limathaysajob@gmail.com>
2024-06-30 23:13:49 +0100

    colocando os MMT e o cortar os espaços em branco

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/string_e_fatiamento.py
@@ -0,0 +1,12 @@
+# Aula 01
+nome = "THaYsa"
+
+print(nome.upper()) # THAYSA
+print(nome.lower()) # thaysa
+print(nome.title()) # Thaysa
+
+texto = "  Olá, mundo!   "
+print(texto)
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
