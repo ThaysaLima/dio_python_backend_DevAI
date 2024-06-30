3e38e890845fb375e18951226196200330368ac4
Thaysa Lima <limathaysajob@gmail.com>
2024-06-30 23:42:48 +0100

    1ª maneira de formatação

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/interacao_de_variaveis.py
@@ -0,0 +1,11 @@
+# Aula 02, Maneiras de interpolação. 
+
+#1ª Maneira, old style
+nome = "Thaysa Lima"
+idade = 20
+profissao = "Programadora"
+cidade = "Fortaleza-CE"
+
+print("Olá, eu sou a %s e tenho %d. Nasci na cidade de %s, e atualmente atuo como %s" %(nome, idade, cidade, profissao)) # %s para valores em string, %d para valores em inteiro, %f para valores em flutuante. 
+
+
