9e44b313c1a3d22077381353163dbf13aaec4cf9
Thaysa Lima <limathaysajob@gmail.com>
2024-06-30 23:47:05 +0100

    2ª maneira de formatação

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/interacao_de_variaveis.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/interacao_de_variaveis.py
@@ -8,4 +8,6 @@ cidade = "Fortaleza-CE"
 
 print("Olá, eu sou a %s e tenho %d. Nasci na cidade de %s, e atualmente atuo como %s" %(nome, idade, cidade, profissao)) # %s para valores em string, %d para valores em inteiro, %f para valores em flutuante. 
 
+#2ª maneira, format. 
 
+print("Olá, e chamo {} e tenho {}. Nasci na cidade de {}, e atualmente atuo como {}".format(nome, idade, cidade, profissao))
