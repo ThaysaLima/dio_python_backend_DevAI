1ada457d2683378d499981356f1957aaab520703
Thaysa Lima <limathaysajob@gmail.com>
2024-07-01 23:02:55 +0100

    Aula 1, listas

---
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/5trabalhando_com_listas_em_python/listas.py
@@ -0,0 +1,18 @@
+# Aula 01 
+
+#ñ é preciso espeficicar o que há na lista, a partir do momento que ela está la dentro ele vira um objeto. Já que python é orientado a objeto. 
+
+frutas = ["melância", "laranja", "maçã"]
+print(frutas)
+
+frutas2 = []
+print(frutas2)
+
+letras = list("Python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
