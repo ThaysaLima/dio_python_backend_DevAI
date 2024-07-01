d9469de06b7d07fd5eb8e223f0187feb4010b2b8
Thaysa Lima <limathaysajob@gmail.com>
2024-07-01 23:28:03 +0100

    Listas, acesso direito e listas aninhadas

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/5trabalhando_com_listas_em_python/listas.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/5trabalhando_com_listas_em_python/listas.py
@@ -16,3 +16,25 @@ print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
+
+# Acesso direto, vamos buscar algo específico da lista. 
+
+# quando vamos buscar da esquerda para a direita, o índice começa por 0.
+# quando vamos buscar da direita para a esquerda, o índice começa por -1. 
+print(frutas[1]) # >>> Laranja
+print(frutas[-3]) # >>> Melância
+
+# Listas aninhadas, listas dentro de listas. 
+
+matriz = [
+
+    [1, "a", 2],
+    ["b", 3, 4], 
+    [5, 6, "c"]
+
+] # Horizontal é linhas e vertical é coluna. 
+
+print(matriz[0]) # >>> [1, "a", 2], vai dar acesso apenas a linha completa.
+print(matriz[0][1]) # >>> "a", o primeiro [] é a linha e o segundo é a coluna. 
+print(matriz[0][-1]) # >>> 2, também podemos usar os índices negativos. 
+print(matriz[])
