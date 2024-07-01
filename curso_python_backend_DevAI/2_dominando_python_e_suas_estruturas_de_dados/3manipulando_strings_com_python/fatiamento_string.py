d37af3126c2937c0ac06632f33c2ec70b8aab3aa
Thaysa Lima <limathaysajob@gmail.com>
2024-07-01 17:08:57 +0100

    Aula 3, forma de pegar a última letra

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/fatiamento_string.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/3manipulando_strings_com_python/fatiamento_string.py
@@ -8,3 +8,5 @@ print(nome[7:15]) # >>> Pereira, isola os strings que vai do 7 ao 15
 print(nome[7:24:2]) # >>> PriadsRi, ele basicmente fala "vai do caractere 7 ao 24 de 2 em 2"
 print(nome[:]) # >>> Thaysa Pereira dos Reis Lima, mostra tudo
 print(nome[::-1]) # >>> amiL sieR sod ariereP asyahT, deixa tudo ao contrário. 
+
+print(nome[-1]) # >>> a, ele vai pegar a última letra.
