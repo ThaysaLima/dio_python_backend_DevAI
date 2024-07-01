1485f6be86b92336ec119477217140ade2617099
Thaysa Lima <limathaysajob@gmail.com>
2024-07-01 17:54:22 +0100

    acrescentando informações sobre o banco

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/4criando_um_sistema_bancario/v1_sistema.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/4criando_um_sistema_bancario/v1_sistema.py
@@ -1,2 +1,6 @@
 # Versão 1, do menu do banco que irá se chamar Banco Py. 
 
+# 3 operações: 1 - Sacar dinheiro, mas so pode ser 3 por dia e até R$500,00. Se não houver saldo o suficiente no banco, então exibir uma mensagem informando que não será possível o saque.
+#              2 - Depositar dinheiro, só pode ser valores inteiros e positivos, e todos os valores que entrar tem que ser armazenado numa variável para depois ser mostrada no extrato do banco. 
+#              3 - Extrato, vai ter que mostras uma lista de todos os depósitos, todos os saque e informar o valor atual da conta. O formato do valor tem que está em R$xxx.xx 
+
