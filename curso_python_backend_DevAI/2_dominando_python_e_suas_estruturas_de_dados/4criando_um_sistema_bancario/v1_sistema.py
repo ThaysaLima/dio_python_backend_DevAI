3302afc8b2d511fe794c26fa47a72861309daca7
Thaysa Lima <limathaysajob@gmail.com>
2024-07-01 18:03:45 +0100

    criando o menu e a base das opções.

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/4criando_um_sistema_bancario/v1_sistema.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/4criando_um_sistema_bancario/v1_sistema.py
@@ -4,3 +4,29 @@
 #              2 - Depositar dinheiro, só pode ser valores inteiros e positivos, e todos os valores que entrar tem que ser armazenado numa variável para depois ser mostrada no extrato do banco. 
 #              3 - Extrato, vai ter que mostras uma lista de todos os depósitos, todos os saque e informar o valor atual da conta. O formato do valor tem que está em R$xxx.xx 
 
+
+inicio_projeto = '''
+    ========================== Bem vindos ao Banco Py ==========================
+
+    1 - Depositar 
+    2 - Sacar 
+    3 - Extrato
+    0 - Sair
+
+    =========== Por favor insira a baixo o número da ação pretendida ===========
+
+'''
+
+print(inicio_projeto)
+
+opcao_bem_vindos = int(input("Digite aqui a opção escolhida: "))
+if opcao_bem_vindos == 1:
+    print("Opção Depositar.")
+elif opcao_bem_vindos == 2:
+    print("Opção Sacar.")
+elif opcao_bem_vindos == 3: 
+    print("Opção Extrato Bancário.")
+elif opcao_bem_vindos == 0: 
+    print("Opção Sair.")
+else:
+    print("Por favor começe novamente e escolha uma opção válida.")
