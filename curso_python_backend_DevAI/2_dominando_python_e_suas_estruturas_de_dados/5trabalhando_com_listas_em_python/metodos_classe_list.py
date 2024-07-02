3de8ee5ba496565b84cd19cba37b9f185b44033c
Thaysa Lima <limathaysajob@gmail.com>
2024-07-02 14:46:44 +0100

    Aula 2: adicionando index e pop, editando algumas linhas.

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/5trabalhando_com_listas_em_python/metodos_classe_list.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/5trabalhando_com_listas_em_python/metodos_classe_list.py
@@ -1,6 +1,6 @@
 # Aula 02
 
-# append: vai adicionar algo na lista, de 1 em 1.
+# a.append(x): vai adicionar algo na lista, de 1 em 1.
 
 lista = []
 
@@ -10,7 +10,7 @@ lista.append([20, 50, 67])
 
 print(lista)
 
-# clear: vai retirar tudo que há dentro da lista e transformar em uma lista vazia. 
+# a.clear(): vai retirar tudo que há dentro da lista e transformar em uma lista vazia. 
 
 lista2 = [1, 'Thaysa Lima', [20, 50, 67]] 
 print(lista2)
@@ -19,7 +19,7 @@ lista2.clear()
 print(lista2)
 
 
-# copy, copiar um objeto mas com ids diferentes.
+# a.copy(), copiar um objeto mas com ids diferentes.
 
 lista3 = [1, 'Thaysa Lima', [20, 50, 67]]
 l3 = lista3.copy()
@@ -30,14 +30,14 @@ print(id(l3), id(lista3)) #São objetos diferentes, o que eu faço em l3 não in
 l3[0] = 2
 print(l3) # >>> [2, 'Thaysa Lima', [20, 50, 67]]
 
-# count: irá contar quantas vezes algo aparece na lista. 
+# a.count(x): irá contar quantas vezes o item aparece na lista. 
 
 cores = ["Azul", "Verde", "Verde", "Vermelho", "Preto", "Vermelho", "Amarelo", "Azul"]
 
 print(cores.count("Vermelho")) # >>> 2. O vermelho apareceu 2 vezes na mesma lista
 print(cores.count("Preto")) # >>> 1
 
-# extend: junção de duas listas, se nessas duas listas tiverem objetos iguais elas vão se repetir quanod juntarem. 
+# a.extend(interável): junção de duas listas, se nessas duas listas tiverem itens iguais elas vão se repetir quanod juntarem. 
 
 linguagens = ["python", "c", "js", "r"]
 
@@ -47,13 +47,19 @@ linguagens.extend(["java", "csharp", "c"])
 
 print(linguagens) # >>> ['python', 'c', 'js', 'r', 'java', 'csharp', 'c']
 
-# index
+# a.index(x[, start[,end]]): irá indicar qual a posição do item na lista. 
 
+linguagens2 = ['python', 'c', 'js', 'r', 'java', 'csharp']
 
+print(linguagens2.index("java")) #4
+print(linguagens2.index("c")) #1
 
-# pop
+# a.pop([i]): remove o item na posição fornecida na lista e retorna. Se nada for especificado ele vai remover o último ítem da lista
 
-
+linguagens3 = ["python", "js", "c", "java", "csharp"]
+print(linguagens3.pop()) # >>> csharp, o último da lista. 
+print(linguagens3.pop(2)) # >>> c, ele vai me retorna a posição 3. 
+print(linguagens3) # >>> ['python', 'js', 'java'], vai me retornar o que restou na lista. 
 
 # remove 
 
