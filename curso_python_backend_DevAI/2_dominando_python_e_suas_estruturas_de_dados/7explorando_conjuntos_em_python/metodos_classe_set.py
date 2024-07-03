8c958792498f10b5a617f7acf6f69e9517565b5f
Thaysa Lima <limathaysajob@gmail.com>
2024-07-03 13:29:28 +0100

    Aula1: métodos da classe SET -> add, clear, copy, discard, pop, remove, len, in

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/7explorando_conjuntos_em_python/metodos_classe_set.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/7explorando_conjuntos_em_python/metodos_classe_set.py
@@ -72,9 +72,68 @@ conj_o = {1,0}
 
 # o conjunto n NÃO tem NADA relacionado com o conjunto m 
 resultado10 = conj_m.isdisjoint(conj_n)
-print(resultado10) #TRUE
+print(resultado10) #TRUE, para chegar ao TRUE não pode haver semelhantes entre os conjuntos.
 
 # o conjunto o tem um item 1 igual ao conjunto m, logo ele está relacionado
 resultado11 = conj_m.isdisjoint(conj_o)
 print(resultado11) #FALSE
+
+# 8) {}.add, adiciona um elemento se ele já não tiver no conjunto
+
+conj_p = {1,2,3}
+conj_p.add(4)
+print(conj_p) # {1, 2, 3, 4}
+
+# 9) {}.copy, cria uma cópia nova, onde essa nova cópia não interfere na original 
+
+conj_q = {1,2,3} # conjunto original
+
+copia_q = conj_q.copy()
+lista_copia_q = list(copia_q)
+lista_copia_q[1] = 6
+
+print(conj_q) # {1, 2, 3}
+print(lista_copia_q) # [1, 6, 3]
+
+# 10) {}.discard, vai descartar o item que for indicado
+
+conj_r = {1,2,4,5}
+conj_r.discard(2) 
+print(conj_r) # {1, 4, 5}
+
+# 11) {}.pop, remove um item na posição fornecida (primeiro transformar em lista) e retorna. Se não for indicado o item ele irá remover o primeiro da lista. 
+
+conj_s = {1,2,3,4,5,6,7,8,9,10}
+
+print(conj_s.pop()) # 1
+print(conj_s) # {2,3,4,5,6,7,8,9,10}
+
+#para remover um item específico eu posso: 
+#1 fazer uma cópia, 2 transformar esse cópia em lista, 3 acessar o item específico para remover através do index. 
+
+copia_conj_s = conj_s.copy()
+lista_copia_conj_s = list(conj_s)
+
+                            #3 = index
+print(lista_copia_conj_s.pop(3)) #5
+print(lista_copia_conj_s) # [2, 3, 4, 6, 7, 8, 9, 10]
+
+# 12) {}.remove, remove um item da lista. 
+
+conj_t = {3,6,9,12}
+conj_t.remove(6)
+print(conj_t) # {9, 3, 12}, ele não retorna em uma ordem. 
+
+# 13) len(), mostra o comprimento
+
+conj_w = {2,4,6,8,10,12,14,16,18}
+print(len(conj_w)) # 9
+
+# 14) in(), verifica se o item pedido está na lista. 
+
+conj_x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+
+print(1 in conj_x) #TRUE
+print(34 in conj_x) #FALSE 
+
 
