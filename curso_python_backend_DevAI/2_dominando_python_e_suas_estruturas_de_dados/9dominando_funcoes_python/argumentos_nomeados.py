e2ad82321daf870459de54efe71520146d66e191
Thaysa Lima <limathaysajob@gmail.com>
2024-07-09 14:35:00 +0100

    Aula 1, Funções Pt. 1

--- curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/9dominando_funcoes_python/argumentos_nomeados.py
+++ curso_python_backend_DevAI/2_dominando_python_e_suas_estruturas_de_dados/9dominando_funcoes_python/argumentos_nomeados.py
@@ -10,4 +10,22 @@ def salvar_carros(marca, modelo, ano, placa):
 
 salvar_carros("fiat", "palio", 1999, "ABC-1234") #desvantagem
 salvar_carros(marca='hyundai', modelo='HB20', ano=2016, placa='DEF-5678') #vantagem
-salvar_carros(**({'marca':'toyota', 'modelo':'SW4', 'ano': 2024, 'placa':'GHI-9101'})) #vantagem
+salvar_carros(**({'marca':'toyota', 'modelo':'SW4', 'ano': 2024, 'placa':'GHI-9101'})) #vantagem, kwargs. 
+
+# *ARGS e **KWARGS
+
+#exemplo fácil 
+
+def funcao_exemplo(**kwargs): 
+    for chave,valor in kwargs.items():
+        print(f"{chave} : {valor}")
+
+funcao_exemplo(nome="Thaysa", idade= 20, cidade= "Fortaleza")
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}:{valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+exibir_poema("Zen of Python", "Beautiful is better than ugly", autor="Tim Peaters", ano= 1999)
