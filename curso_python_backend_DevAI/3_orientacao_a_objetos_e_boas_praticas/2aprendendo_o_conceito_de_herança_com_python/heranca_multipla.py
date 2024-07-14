28fe15a46fdb15f6883ecec52a232cbddba099e7
Thaysa Lima <limathaysajob@gmail.com>
2024-07-14 01:57:24 +0100

    Inicializador

---
+++ curso_python_backend_DevAI/3_orientacao_a_objetos_e_boas_praticas/2aprendendo_o_conceito_de_herança_com_python/heranca_multipla.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, nmr_patas):
+        self.nmr_patas = nmr_patas
+        
+
+
+class Mamifero(Animal):
+    def __init__(self, nmr_patas):
+        self.nmr_patas = nmr_patas
+
+
+class Ave(Animal):
+    def __init__(self, nmr_patas):
+        self.nmr_patas = nmr_patas
+
+
+class Gato(Mamifero):
+    pass
+
+
+class Onitorrinco(Mamifero, Ave):
+    pass
