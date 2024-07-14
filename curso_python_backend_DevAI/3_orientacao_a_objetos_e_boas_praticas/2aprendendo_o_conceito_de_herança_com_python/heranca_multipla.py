f31ebd22c4f599bbcfb333708e105d147aac0d13
Thaysa Lima <limathaysajob@gmail.com>
2024-07-14 02:01:35 +0100

    Adicionando o str para estilizar o formato do print

--- curso_python_backend_DevAI/3_orientacao_a_objetos_e_boas_praticas/2aprendendo_o_conceito_de_herança_com_python/heranca_multipla.py
+++ curso_python_backend_DevAI/3_orientacao_a_objetos_e_boas_praticas/2aprendendo_o_conceito_de_herança_com_python/heranca_multipla.py
@@ -1,6 +1,9 @@
 class Animal:
     def __init__(self, nmr_patas):
         self.nmr_patas = nmr_patas
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
         
 
 
